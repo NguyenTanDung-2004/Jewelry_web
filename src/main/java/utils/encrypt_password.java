2b7a8f33d0010793eecd6c90d89bc9f3ecd43ddd
Nguyen Tan Dung <22520001@gm.uit.edu.vn>
2024-05-18 08:34:26 +0700

    update

--- src/main/java/utils/encrypt_password.java
+++ src/main/java/utils/encrypt_password.java
@@ -1,20 +1,22 @@
 package utils;
 
+import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
+import java.util.Base64;
 
 public class encrypt_password {
 	private static String add = "asdfasdfasd;@asdfasdfasd.?";
 	public static String ecrypt_to_SHA1(String str) {
 		String result = null;
-		String s = str + add;
-		try {
-			byte[] data_to_byte = s.getBytes("UTF-8");
-			MessageDigest md = MessageDigest.getInstance("SHA-1");
-			result = org.apache.tomcat.util.codec.binary.Base64.encodeBase64String(data_to_byte);
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
-		return result;
+        String s = str + add;
+        try {
+            byte[] dataToByte = s.getBytes(StandardCharsets.UTF_8);
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            byte[] hashBytes = md.digest(dataToByte);
+            result = Base64.getEncoder().encodeToString(hashBytes);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return result;
 	}
 }
